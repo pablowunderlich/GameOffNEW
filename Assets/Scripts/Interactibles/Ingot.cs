80c7ed53cbce59c813812d10b58b35234e34bb45
lars-betting <89662589+lars-betting@users.noreply.github.com>
2023-11-07 14:26:19 +0000

    + Ingots + GameManager

---
+++ Assets/Scripts/Interactibles/Ingot.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingot : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody2D _rb;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>())
+        {
+            GameManager.Instance.SetScore(GameManager.Instance.GetScore() + 1);
+            Debug.Log(GameManager.Instance.GetScore());
+            Destroy(gameObject);
+        }
+    }
+}
