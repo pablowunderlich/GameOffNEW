8302c38de28a8e0f7168dc1fb52b1ffb3527c5b5
lars-betting <89662589+lars-betting@users.noreply.github.com>
2023-11-07 14:50:32 +0000

    Revert "Revert "Revert "Merge branch 'main' into Programming"""
    This reverts commit 083f9e2cd66390642e1574106ff607337fd84928.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -17,22 +17,14 @@ namespace AK
         static const AkUniqueID PLAY_ENEMY_TURRET_FIRE = 616075970U;
         static const AkUniqueID PLAY_PLAYER_ARM_SWING = 1726909165U;
         static const AkUniqueID PLAY_PLAYER_BUTTON_PRESS = 128901360U;
-        static const AkUniqueID PLAY_PLAYER_FLOOR_HIT = 3978617892U;
         static const AkUniqueID PLAY_PLAYER_JUMP = 562256996U;
         static const AkUniqueID PLAY_PLAYER_LAND = 4249207015U;
         static const AkUniqueID PLAY_PLAYER_PRESS_ON_PAD = 3228691145U;
         static const AkUniqueID PLAY_PLAYER_PULL_LEVER = 2186439036U;
         static const AkUniqueID PLAY_PLAYER_TAKE_DAMAGE = 1172205655U;
-        static const AkUniqueID PLAY_PLAYER_VOICE = 3216481374U;
-        static const AkUniqueID PLAY_PLAYER_WALLJUMP = 937438432U;
         static const AkUniqueID STOP_DASH_GLIDE_LOOP_SFX_4 = 3432329370U;
         static const AkUniqueID STOP_PLAYER_JUMP = 3588747354U;
     } // namespace EVENTS
-
-    namespace GAME_PARAMETERS
-    {
-        static const AkUniqueID PLAYER_MAX_JUMP = 3305153192U;
-    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
